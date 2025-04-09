a475434ae4735c010f72a8ccca9686a51130eddd
noahapursell <noahapursell@gmail.com>
2025-04-09 11:17:56 -0500

    added docker deployment support

---
+++ scripts/windows/deploy-smart-contracts.bat
@@ -0,0 +1,1 @@
+docker-compose -f smart-contracts\docker-compose.yml up -d --build
--- smart-contracts/hardhat.config.ts
+++ smart-contracts/hardhat.config.ts
@@ -7,6 +7,11 @@ import "@nomicfoundation/hardhat-ignition-ethers";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    localhost: {
+      url: "http://hardhat-node:8545", // <-- use container name, not 127.0.0.1
+    },
+  },
 };
 
 export default config;
