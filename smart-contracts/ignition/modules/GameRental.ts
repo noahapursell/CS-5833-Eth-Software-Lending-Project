fb7bb8e731f23013ca14cd1988211c1fc33cc724
noahapursell <noahapursell@gmail.com>
2025-04-17 18:46:56 -0500

    update ignition script

--- smart-contracts/ignition/modules/GameRental.ts
+++ smart-contracts/ignition/modules/GameRental.ts
@@ -11,8 +11,8 @@ export default buildModule("GameRentalSetup", (m) => {
     const owner3 = m.getAccount(3);
 
     // Developer registers games
-    const regGame1 = m.call(gameRental, "registerGame", [1, 1000000000000000000n, 1000n, 3600n, 1000n], { from: developer, id: "registerGame1" });
-    const regGame2 = m.call(gameRental, "registerGame", [2, 2000000000000000000n, 1200n, 7200n, 1000n], { from: developer, id: "registerGame2" });
+    const regGame1 = m.call(gameRental, "registerGame", [1, 1000000000000000000n, 1000n, 1000n], { from: developer, id: "registerGame1" });
+    const regGame2 = m.call(gameRental, "registerGame", [2, 2000000000000000000n, 1200n, 1000n], { from: developer, id: "registerGame2" });
 
     // Owners buy games
     m.call(gameRental, "buyGame", [1], { from: owner1, value: 1000000000000000000n, id: "buyGame1", after: [regGame1] });
